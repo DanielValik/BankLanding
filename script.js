bd458b6257867ecdf0766b45ac041d02cc57a933
DanielValik <danilvalik@gmail.com>
2023-10-20 20:53:28 +0200

    Sticky nav + scroll appearns + lazy load + slider

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ document.addEventListener("keydown", function (e) {
 // Cookie message
 
 let header = document.querySelector("header");
-let cookieMessage = document.createElement("div");
-cookieMessage.innerHTML = `Czy zezwalasz dostep dla plikow cookie? <button id="ok_cookie">Ok</button>`;
+// let cookieMessage = document.createElement("div");
+// cookieMessage.innerHTML = `Czy zezwalasz dostep dla plikow cookie? <button id="ok_cookie">Ok</button>`;
 
-header.before(cookieMessage);
+// header.before(cookieMessage);
 
-document
-  .getElementById("ok_cookie")
-  .addEventListener("click", () => cookieMessage.remove());
+// document
+//   .getElementById("ok_cookie")
+//   .addEventListener("click", () => cookieMessage.remove());
 
 // console.log(Number.parseFloat(getComputedStyle(cookieMessage).height));
 
@@ -102,7 +102,7 @@ tabContainer.addEventListener("click", function (e) {
     .classList.add("operations__content--active");
 });
 
-// Navigation
+// Navigation Hover effects
 
 const nav = document.querySelector(".nav");
 
@@ -133,4 +133,141 @@ window.addEventListener("scroll", function () {
   } else {
     nav.classList.remove("sticky");
   }
+});
+
+// Sticky Navigation
+
+const navHeight = nav.getBoundingClientRect().height;
+
+let headerObserver = new IntersectionObserver(
+  function (entries) {
+    if (!entries[0].isIntersecting) {
+      nav.classList.add("sticky");
+    } else {
+      nav.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+  }
+);
+
+headerObserver.observe(header);
+
+// Scroll appearance
+
+const sections = document.querySelectorAll("section");
+const appearanceObserver = new IntersectionObserver(
+  (entries, observer) => {
+    if (!entries[0].isIntersecting) return;
+
+    entries[0].target.classList.remove("section--hidden");
+    observer.unobserve(entries[0].target);
+  },
+  {
+    root: null,
+    threshold: 0.2,
+  }
+);
+
+sections.forEach((s) => {
+  s.classList.add("section--hidden");
+  appearanceObserver.observe(s);
+});
+
+// Lazy loading
+
+const lazyImages = document.querySelectorAll("img[data-src]");
+const lazyImgObserver = new IntersectionObserver(
+  function (entries, observer) {
+    if (!entries[0].isIntersecting) return;
+
+    entries[0].target.src = entries[0].target.dataset.src;
+    entries[0].target.addEventListener("load", () => {
+      entries[0].target.classList.remove("lazy-img");
+    });
+    observer.unobserve(entries[0].target);
+  },
+  {
+    root: null,
+    threshold: 0.1,
+  }
+);
+
+lazyImages.forEach((lzi) => {
+  lazyImgObserver.observe(lzi);
+});
+
+// Slider
+
+const slides = document.querySelectorAll(".slide");
+const sliderBtnLeft = document.querySelector(".slider__btn--left");
+const sliderBtnRight = document.querySelector(".slider__btn--right");
+let sliderLenght = slides.length - 1;
+let currentSlide = 0;
+
+function setSliderTranslateX() {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+  });
+}
+
+setSliderTranslateX();
+
+function goLeft() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = sliderLenght;
+  }
+
+  setSliderTranslateX();
+  activateCurrentDot();
+}
+
+function goRight() {
+  currentSlide++;
+  if (currentSlide > sliderLenght) {
+    currentSlide = 0;
+  }
+
+  setSliderTranslateX();
+  activateCurrentDot();
+}
+
+sliderBtnLeft.addEventListener("click", goLeft);
+sliderBtnRight.addEventListener("click", goRight);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") goLeft();
+  if (e.key === "ArrowRight") goRight();
+});
+
+// Slider dots
+
+const dots = document.querySelector(".dots");
+
+slides.forEach((_, index) => {
+  dots.insertAdjacentHTML(
+    "beforeend",
+    `<button class="dots__dot" data-slide ="${index}" ></button>`
+  );
+});
+
+function activateCurrentDot() {
+  document.querySelectorAll(".dots__dot").forEach((dot) => {
+    dot.classList.remove("dots__dot--active");
+  });
+  document
+    .querySelector(`.dots__dot[data-slide = "${currentSlide}"]`)
+    .classList.add("dots__dot--active");
+}
+
+activateCurrentDot();
+
+dots.addEventListener("click", (e) => {
+  currentSlide = e.target.dataset.slide;
+
+  setSliderTranslateX();
+  activateCurrentDot();
 });
